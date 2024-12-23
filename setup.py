55b8458028815e19213ee86f513827ca0d8af45d
eluzquadros <eluzquadros@gmail.com>
2024-12-23 13:04:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
     name="SICAR",
     version="0.0.1",
     packages=find_packages(),
+    python_requires=">=3.6",
     install_requires=[
         'aiohttp',
         'aiohttp_socks',
         'beautifulsoup4',
         'pillow',
         'tqdm'
-    
+    ],
+    package_data={'': ['*']},
+    zip_safe=False
+)
