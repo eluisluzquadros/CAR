14cabf4b22ce8da916d71f6c294447cf182848a6
eluzquadros <eluzquadros@gmail.com>
2024-12-23 12:44:16 -0300

    Update setup.py
    add aiohttp

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="SICAR",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
-        'httpx',
-        'Pillow',
-        'numpy',
-        'opencv-python',
+        'aiohttp',
+        'aiohttp_socks',
         'beautifulsoup4',
-        'tqdm',
-        'pytesseract'
-    ],
-)
+        'pillow',
+        'tqdm'
+    
