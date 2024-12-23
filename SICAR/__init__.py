5a0404eac62900f9624a6bdf6541d6d6fa5b58a8
eluzquadros <eluzquadros@gmail.com>
2024-12-23 13:21:35 -0300

    Update __init__.py

--- SICAR/__init__.py
+++ SICAR/__init__.py
@@ -1,7 +1,2 @@
 """SICAR - Tool designed for students, researchers, data scientists or anyone who would like to have access to SICAR files."""
 
-# SICAR/drivers/__init__.py
-from .captcha import Captcha
-from .tesseract import Tesseract
-
-__all__ = ['Captcha', 'Tesseract']
