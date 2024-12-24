de8414063c49c48ecfa5b58308ee7e456322f7a3
eluzquadros <eluzquadros@gmail.com>
2024-12-23 22:37:35 -0300

    update init
    update

--- SICAR/drivers/__init__.py
+++ SICAR/drivers/__init__.py
@@ -1,7 +1,7 @@
-# SICAR/drivers/__init__.py
+# /content/CAR/SICAR/drivers/__init__.py
 """SICAR drivers for captcha processing."""
 
 from .captcha import Captcha, CaptchaProcessingError
 from .tesseract import Tesseract
 
-__all__ = ['Captcha', 'Tesseract', 'CaptchaProcessingError']
+__all__ = ['Captcha', 'CaptchaProcessingError', 'Tesseract']
