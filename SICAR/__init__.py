a9bff3e5a5efb831b64a9577205c3704d4258e94
eluzquadros <eluzquadros@gmail.com>
2024-12-23 22:37:12 -0300

    update init
    update

--- SICAR/__init__.py
+++ SICAR/__init__.py
@@ -1,7 +1,35 @@
-# SICAR/drivers/__init__.py
-"""SICAR drivers for captcha processing."""
+# /content/CAR/SICAR/__init__.py
+"""SICAR - Tool designed for students, researchers, data scientists or anyone who would like to have access to SICAR files."""
 
-from .captcha import Captcha, CaptchaProcessingError
-from .tesseract import Tesseract
+from .state import State
+from .polygon import Polygon
+from .url import Url
+from .exceptions import (
+    UrlNotOkException,
+    PolygonNotValidException,
+    StateCodeNotValidException,
+    FailedToDownloadCaptchaException,
+    FailedToDownloadPolygonException,
+    FailedToGetReleaseDateException
+)
+from .drivers.captcha import Captcha, CaptchaProcessingError
+from .drivers.tesseract import Tesseract
+from .sicar import Sicar
 
-__all__ = ['Captcha', 'Tesseract', 'CaptchaProcessingError']
+__version__ = '0.0.1'
+
+__all__ = [
+    'Sicar',
+    'State',
+    'Polygon',
+    'Url',
+    'Captcha',
+    'Tesseract',
+    'CaptchaProcessingError',
+    'UrlNotOkException',
+    'PolygonNotValidException',
+    'StateCodeNotValidException',
+    'FailedToDownloadCaptchaException',
+    'FailedToDownloadPolygonException',
+    'FailedToGetReleaseDateException'
+]
